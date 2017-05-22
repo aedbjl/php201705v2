003532bac156bc7ea252d4b25892f6b6be1103c5
HAHAHA <hiinetoeo@gmail.com>
2017-05-22 09:00:37 +0800

    ddddd

--- HW02 20170519year.php
+++ HW02 20170519year.php
@@ -9,7 +9,7 @@ $x = $_GET['year'];
     $r = $x % 4000;
     if ($x) {
         if ($r == 0) {
-            echo '不潤阿 傻了嗎?';
+            echo '不潤阿';
         } else if ($r != 0) {
             $s = $x % 400;
             if ($s == 0) {
@@ -17,13 +17,13 @@ $x = $_GET['year'];
             } else if ($s != 0) {
                 $y = $x % 100;
                 if ($y == 0) {
-                    echo '不潤阿 傻了嗎?';
+                    echo '不潤阿';
                 } else if ($y != 0) {
                     $p = $x % 4;
                     if ($p == 0) {
                         echo '潤阿';
                     } else {
-                        echo '不潤阿 傻了嗎?';
+                        echo '不潤阿';
                     }
                 }
             }
